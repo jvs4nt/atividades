087c4d9d43fcfe2b00313607b0d9be55b210a43b
joaosantos <joaovitor.d370@gmail.com>
2023-08-21 14:15:09 -0300

    peso

---
+++ java/peso/peso.java
@@ -0,0 +1,39 @@
+package peso;
+
+import java.util.Scanner;
+
+public class peso {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double pesoIdeal;
+
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("calcular peso ideal");
+        System.out.println("-------------------------------");
+        System.out.println(" ");
+
+        System.out.println("digite a sua altura: ");
+        double altura = scanner.nextDouble();
+
+        System.out.println(" ");
+
+        System.out.println("digite seu sexo: ");
+        System.out.println("(1 - FEMININO / 2 - MASCULINO)");
+        int sexo = scanner.nextInt();
+
+        switch (sexo) {
+            case 1:
+            pesoIdeal = (62.1 * altura) - 44.7;
+            System.out.println("peso ideal: " + pesoIdeal);
+            break;
+
+            case 2:
+            pesoIdeal = (72.7 * altura) - 58;
+            System.out.println("peso ideal: " + pesoIdeal);
+            break;
+            
+        }
+    }
+}
