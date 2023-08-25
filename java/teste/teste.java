668c46bd2839ec8cae27a4917136e0f3aeb7ee05
joaosantos <joaovitor.d370@gmail.com>
2023-08-25 16:08:33 -0300

    ajuste

---
+++ java/teste/teste.java
@@ -0,0 +1,18 @@
+package teste;
+
+import java.util.Scanner;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("digite uma frase");
+        String frase = scanner.nextLine();
+        
+        System.out.println(" ");
+        System.out.println("sua frase foi: " + frase);
+    }
+} 
+    
+
