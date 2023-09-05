5cb14101397362b7b1ad695371e4e47746b1363b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-05 19:27:03 -0300

    python files

---
+++ python/ex1.py
@@ -0,0 +1,7 @@
+valor = float(input("digite o valor da mercadoria: "))
+valor_maos = float(input("digite o valor que tem em maos: "))
+
+if (valor_maos >= valor):
+    print ("o valor em maos é suficiente")
+else:
+    print ("o valor em maos nao é suficiente")
