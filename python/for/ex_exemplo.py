0841d0c527fb89634d5ce5a7c370789ae7e95f91
joaosantos <joaovitor.d370@gmail.com>
2023-09-12 20:13:41 -0300

    exercicios for

---
+++ python/for/ex_exemplo.py
@@ -0,0 +1,51 @@
+# exibir todos os numeros entre 1 e 10, inclusive
+
+print("numeros de 1 a 10: ")
+for i in range(1, 11):
+    print(i)
+
+###
+print("")
+###
+
+
+# exibir os numeros de 0 a 9
+
+print("numeros de 1 a 9: ")
+for i in range(10):
+    print (i)
+
+###
+print("")
+###
+
+# exibir todos os numeros de 2 ate 20, de 2 e 2 (inicio, fim, passo)
+
+print("numeros de 1 a 20(2 em 2): ")
+for i in range(2, 21, 2):
+    print(i)
+    
+###
+print("")
+###
+
+# exibir o exercicio de cima mas sem colocar o passo
+
+print("numeros de 1 a 20(2 em 2) utilizando if:  ")
+for i in range(1, 21):
+    if (i % 2 == 0):
+        print(i)
+
+###
+print("")
+###
+
+# exibir todos os numeros de 10 ate 1
+
+print("numeros de 10 a 1: ")
+for i in range(10, 0, -1):
+    print(i)
+    
+###
+print("")
+###
---
+++ python/for/ex_exemplo2.py
@@ -0,0 +1,31 @@
+###
+# print(" ")
+###
+
+# somatorio de todos os numeros de 1 a 10
+
+# soma = 0
+
+# for i in range(1, 11):
+#     soma = soma + i
+    
+# print (f"Soma: ", soma)
+
+###
+print(" ")
+###
+
+# somatorio de 5 numeros reais definidos pelo usuario
+
+soma = 0
+
+for i in range(1, 6):
+    num = float(input(f"digite o numero {i}:"))
+    soma = soma + num
+    
+print (f"Soma: ", soma)
+
+###
+print(" ")
+###
+
